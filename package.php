d8f8f94652f548d0195903acb5e7a1e86eea5831
KUBO Atsuhiro <kubo@iteman.jp>
2007-07-14 12:59:43 +0000

    - A minor change.

--- package.php
+++ package.php
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Piece_Unity
- * @subpackage  Piece_Unity_Component_Authentication
+ * @subpackage Piece_Unity_Component_Authentication
  * @copyright  2006-2007 KUBO Atsuhiro <iteman@users.sourceforge.net>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
  * @version    SVN: $Id$
